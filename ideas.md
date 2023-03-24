27fb3c01f3d89c060511b64941fd8876563250a0
Ahmed-Eldalatony <108447999+Ahmed-Eldalatony@users.noreply.github.com>
2023-03-24 18:10:58 +0200

    Add files via upload

---
+++ ideas.md
@@ -0,0 +1,7 @@
+what if it is a single page app when you press on a link it expands and take the whole page
+
+- need to redirect the sky host website
+- think of changing the font color
+- what should be the hover effect in the lis
+- delete the font awesome library file with cdnjs
+- multiple fonts?
---
+++ main.js
@@ -0,0 +1,37 @@
+let mode = document.querySelector(".mode")
+let sudo=document.querySelectorAll(".sudo")
+// let dynamicIsland = document.querySelector(".nav-cobtn-contntainer")
+let glassF=document.querySelectorAll(".glass-f")
+let navContainer = document.querySelector(".nav-container nav")
+let btnCont = document.querySelector(".btn-cont .btn")
+let lis = document.querySelectorAll("header nav ul li")
+mode.addEventListener("click",function(){
+    console.log(mode)
+    sudo.forEach((el)=>{
+        el.classList.toggle("light-on")
+    
+        glassF.forEach(ele=>{
+            if(el.classList.contains("light-on")){
+                ele.style.boxShadow="0 0px 1.5rem var(--primary-color)"
+            }
+        else{
+            ele.style.boxShadow="0 0 0 0 "
+            // navContainer.style.boxShadow="0 0 0 0"
+    
+            }
+        })
+            // btnCont.style.boxShadow="0 0px 1.5rem var(--primary-color)"
+            //  navContainer.style.boxShadow="0 0px 1.5rem var(--primary-color)"
+    })
+})
+
+setTimeout(() => {
+    
+    lis.forEach(el=>el.style.display="block")
+},900)
+let loadingSc= document.querySelector(".loading-s")
+let loadingItem= document.querySelector(".loading-i")
+
+
+
+
